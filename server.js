8c2425f8a24541dace30d570d10d5a62f8327a78
WandersonGomes <dersom100@gmail.com>
2023-10-29 12:44:19 -0300

    Configurando handlebars.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const { engine } = require('handlebars');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.listen(port, () => {
     console.log(`Servidor está rodando em http://localhost:${port}`);
