e4553ae80b49413ca822e48afb25c73e83554eba
isaac <isaac.sa.silva08@aluno.ifce.edu.br>
2023-11-02 17:35:37 -0300

    Atualizando rota customers para listar os Clientes

--- routers/admin.js
+++ routers/admin.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const { isAdmin } = require('../middlewares/autenticacao');
+const { Customer } = require('../database/models')
 
-const { config_dashboard_page, config_about_page } = require('../config/pages');
+const { config_dashboard_page, config_about_page, config_customers_page } = require('../config/pages');
 
 router.get('/dashboard', isAdmin, (req, res) => {
     res.render('dashboard', config_dashboard_page);
     config_dashboard_page.clearMessages();
 });
 
-router.get('/customers', isAdmin, (req, res) => {
-    res.render('customers');
+router.get('/customers', async (req, res) => {
+    const customers = await Customer.findAll({
+        attributes: ['id', 'name', 'lastName', 'cpf']
+    });
+
+    const customersJSON = customers.map(customer => customer.toJSON())
+    config_customers_page.customers = customersJSON;
+
+    console.log(customersJSON)
+
+    res.render('customers', config_customers_page);
+    
 });
 
 router.get('/about', isAdmin, (req, res) => {
