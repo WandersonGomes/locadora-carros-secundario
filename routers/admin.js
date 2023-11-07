7dda8046fb806d5667b385774ac866a13446cb2d
WandersonGomes <dersom100@gmail.com>
2023-11-07 19:48:55 -0300

    Pequenos upgrades no dashboard.

--- routers/admin.js
+++ routers/admin.js
@@ -2,19 +2,26 @@ const express = require('express');
 const router = express.Router();
 const { isAdmin } = require('../middlewares/autenticacao');
 const routerCustomer = require('./customers');
-const routerCars = require('./cars')
+const routerCars = require('./cars');
+
+const { Customer, Car } = require('../database/models');
 
 const { config_dashboard_page, config_about_page} = require('../config/pages');
 
 router.use('/customers', routerCustomer);
 router.use('/cars', routerCars);
 
-router.get('/dashboard', isAdmin, (req, res) => {
+router.use(isAdmin);
+
+router.get('/dashboard', async (req, res) => {
+    config_dashboard_page.amount_cars_enable = await Car.count({where: {status: 'disponivel'}});
+    config_dashboard_page.amount_customers = await Customer.count();
+
     res.render('dashboard', config_dashboard_page);
     config_dashboard_page.clearMessages();
 });
 
-router.get('/about', isAdmin, (req, res) => {
+router.get('/about', (req, res) => {
     res.render('about', config_about_page);
 });
 
